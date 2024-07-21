8d58e3fdc050b98a36d8136d6184f68c3a638736
ARJUNA1996 <amallikarjuna90@gmail.com>
2024-07-21 10:38:22 +0530

    'added'

---
+++ joblesspeople/src/main/java/com/mack/joblesspeople/JoblesspeopleApplication.java
@@ -0,0 +1,13 @@
+package com.mack.joblesspeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoblesspeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoblesspeopleApplication.class, args);
+	}
+
+}
